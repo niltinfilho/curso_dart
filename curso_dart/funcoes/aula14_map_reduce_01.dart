55abe5beb3ad0c8bcb90397d65ee9413211661b3
Nilton Filho <niltonfilho115@gmail.com>
2024-04-28 21:41:05 -0300

    primeira aula sobre map & reduce

---
+++ curso_dart/funcoes/aula14_map_reduce_01.dart
@@ -0,0 +1,17 @@
+main() {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+
+  String Function(Map) pegarONome = (aluno) => aluno['nome'];
+  int Function(String) qtdDeLetras = (texto) => texto.length;
+  var nomes = alunos.map(pegarONome);
+  var qtdsDeLetras = nomes.map(qtdDeLetras);
+  print(nomes);
+  print(qtdsDeLetras);
+}
