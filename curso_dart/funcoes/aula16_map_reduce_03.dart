40d6d2fb81dad04040581cb9a153f3e46f748fa4
Nilton Filho <niltonfilho115@gmail.com>
2024-06-10 21:26:14 -0300

    finalizando o modulo de funcoes

--- curso_dart/funcoes/aula16_map_reduce_03.dart
+++ curso_dart/funcoes/aula16_map_reduce_03.dart
@@ -8,9 +8,17 @@ main() {
     {'nome': 'Ricardo', 'nota': 6.8},
   ];
 
-  var notas = alunos.map((aluno) => aluno['nota']);
+  var keys = alunos.map((aluno) => aluno['nota']);
   // Usa a funcao map para buscar apenas as chaves da lista de elementos
+  print(keys);
+
+  var notas = keys.map((nota) => (nota as double));
+  // Usa a funcao map para retornar as chaves no tipo double
   print(notas);
 
+  var total = notas.reduce((t, a) => t + a);
+  // Usa a funcao reduce para retornar a soma de todas as notas
+  print(total);
 
+  print('O valor da media eh: ${total / alunos.length}');
 }
