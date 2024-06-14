f4d8005e6a12753b2aac59e7593dc51a34c6a36b
Nilton Filho <niltonfilho115@gmail.com>
2024-06-14 00:03:28 -0300

    iniciando as anotacoes de orientacao a objetos

--- curso_dart/oo/aula01_classe_data.dart
+++ curso_dart/oo/aula01_classe_data.dart
@@ -1,3 +1,15 @@
 class Data {
+  late int dia;
+  late int mes;
+  late int ano;
+}
 
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 3;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2020;
+
+  print(dataAniversario);
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
 }
